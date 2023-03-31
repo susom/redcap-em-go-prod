f869914ebb08a1bee36d1e3d0ca4228cf41d9550
ihabz <ihab.zeedia@stanford.edu>
2023-03-31 11:00:50 -0700

    add debug

--- GoProd.php
+++ GoProd.php
@@ -109,6 +109,7 @@ class GoProd extends \ExternalModules\AbstractExternalModule
     public function redcap_module_ajax($action, $payload, $project_id, $record, $instrument, $event_id, $repeat_instance,
                                        $survey_hash, $response_id, $survey_queue_hash, $page, $page_full, $user_id, $group_id)
     {
+        $this->emDebug('Action: ' . $action);
         if ($action == self::ALL_VALIDATIONS) {
             $result = [];
             foreach ($this->getValidations()->getEnabledRules() as $name => $validation) {
