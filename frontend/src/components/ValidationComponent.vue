76951e1c75140c42c7e9538419dbbcd8fb9b1246
ihabz <ihab.zeedia@stanford.edu>
2023-03-31 11:13:32 -0700

    make modal large.

--- frontend/src/components/ValidationComponent.vue
+++ frontend/src/components/ValidationComponent.vue
@@ -123,7 +123,7 @@
     <!-- Modal -->
     <div class="modal fade" id="ruleModal" ref="ruleModal" tabindex="-1" aria-labelledby="ruleModalLabel"
          aria-hidden="true">
-      <div class="modal-dialog">
+      <div class="modal-dialog modal-lg">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">{{ modalObject.title }}</h5>
