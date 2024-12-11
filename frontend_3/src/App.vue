1ab4fa3fe8d2dbfdd87954cef67972bf5274db57
ihabz <ihab.zeedia@stanford.edu>
2024-12-11 11:10:02 -0800

    upgrade vue2 to vue3

---
+++ frontend_3/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+   <PageHeader class="mb-2"/>
+  <ValidationComponent/>
+</template>
+
+<script>
+import PageHeader from "./components/PageHeader.vue";
+import ValidationComponent from "./components/ValidationComponent.vue";
+
+export default {
+  name: 'App',
+  components: {
+    PageHeader,
+    ValidationComponent
+  }
+}
+</script>
+
